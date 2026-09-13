c5e5f6b0804bc1aa3d59a7c9a63f768883bf1e87
Chay <cahyadi.nugraha@temadata.id>
2026-09-14 06:36:11 +0700

    test(engine): verify log replay fidelity against ubuntu22 production nginx logs

--- tests/replay_fidelity_test.rs
+++ tests/replay_fidelity_test.rs
@@ -58,3 +58,36 @@ fn test_replay_fidelity_ssh_evaluation() {
     let threats_threshold = replay_log_file(temp.path(), &[]).unwrap();
     assert_eq!(threats_threshold, 2);
 }
+
+#[test]
+fn test_replay_fidelity_ubuntu22_reference_logs() {
+    let mut temp = tempfile::NamedTempFile::new().unwrap();
+
+    writeln!(
+        temp,
+        r#"45.194.92.67 - - [09/Sep/2026:00:09:05 +0700] "GET / HTTP/1.1" 302 138 "http://46.250.231.252:80/" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36""#
+    ).unwrap();
+    writeln!(
+        temp,
+        r#"47.250.114.23 - - [09/Sep/2026:02:41:37 +0700] "GET / HTTP/1.1" 302 138 "-" "curl/7.74.0""#
+    ).unwrap();
+
+    let clean_threats = replay_log_file(temp.path(), &[]).unwrap();
+    assert_eq!(clean_threats, 0);
+
+    writeln!(
+        temp,
+        r#"20.151.10.161 - - [09/Sep/2026:03:45:59 +0700] "GET /f35.update.php HTTP/1.1" 404 142 "-" "-""#
+    ).unwrap();
+
+    let one_threat = replay_log_file(temp.path(), &[]).unwrap();
+    assert_eq!(one_threat, 1);
+
+    writeln!(
+        temp,
+        r#"16.5.0.236 - - [09/Sep/2026:00:50:18 +0700] "POST /boaform/admin/formLogin HTTP/1.1" 404 142 "-" "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0""#
+    ).unwrap();
+
+    let two_threats = replay_log_file(temp.path(), &[]).unwrap();
+    assert_eq!(two_threats, 2);
+}
